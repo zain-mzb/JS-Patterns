6c8203abcaa3c651de0ae55816f614d7ee4de32e
Zain <72197346+zain-mzb@users.noreply.github.com>
2023-01-19 17:19:12 +0500

    Add files via upload

---
+++ factory.js
@@ -0,0 +1,36 @@
+function Developer(name){
+    this.name = name
+    this.type = "Developer"
+} 
+
+function Tester(name){
+    this.name = name
+    this.type = "Tester"
+}
+
+function EmployeeFactory(){
+    this.create = (name, type)=>{
+        switch(type){
+            case 1:
+                return new Developer(name)
+            case 2:
+                return new Tester(name)
+        }
+    }
+}
+
+function say(){
+console.log("Hi, I am " + this.name + " and I am a " + this.type)    
+}
+
+const employeeFactory = new EmployeeFactory()
+
+const employees =[]
+
+
+employees.push(employeeFactory.create("Patrick", 1))
+employees.push(employeeFactory.create("John", 2))
+
+employees.forEach(emp=>{
+    say.call(emp)
+})
