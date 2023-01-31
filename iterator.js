aba7474a5830444e9632ded55553ac62510fc884
Zain <72197346+zain-mzb@users.noreply.github.com>
2023-01-31 15:16:41 +0500

    Add files via upload

---
+++ iterator.js
@@ -0,0 +1,19 @@
+const items = [1, "zain", false, 1.24];
+
+function Iterator(items) {
+  this.items = items;
+  this.index = 0;
+}
+
+Iterator.prototype = {
+  hasNext: function () {
+    return this.index < this.items.length
+  },
+  next: function () {
+    return this.items[this.index++]
+  },
+};
+
+const iter = new Iterator(items)
+while(iter.hasNext())
+  console.log(iter.next())
